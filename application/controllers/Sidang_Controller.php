4dfb1f5791cfd348f0f2d2fd98d54fe892e9bf82
Rezky <rezky20ti@mahasiswa.pcr.ac.id>
2024-07-24 11:25:43 +0700

    Controller Udpate

--- application/controllers/Sidang_Controller.php
+++ application/controllers/Sidang_Controller.php
@@ -16,19 +16,19 @@ class Sidang_Controller extends CI_Controller {
         $data = json_decode(file_get_contents('php://input'), true);
 
         $data_mhs = $this->Mahasiswa_model->get_by_nim($data['mahasiswa']);
-        $dosen_pbb = $this->Dosen_model->get_by_name($data['dosen_pbb']);
-        $dosen_pnj1 = $this->Dosen_model->get_by_name($data['dosen_pnj1']);
-        $dosen_pnj2 = $this->Dosen_model->get_by_name($data['dosen_pnj2']);
+        $dosen_pbb = $this->Dosen_model->get_by_id($data['dosen_pbb']);
+        $dosen_pnj1 = $this->Dosen_model->get_by_id($data['dosen_pnj1']);
+        $dosen_pnj2 = $this->Dosen_model->get_by_id($data['dosen_pnj2']);
         $ruangan = $this->Ruangan_model->get_all();
 
-        $kelas_mhs = $this->Kelas_model->get_by_id($data_mhs['id_kelas']);
-        $jadwal_kelas = $kelas_mhs['jadwal'];
+        $kelas_mhs = $this->Kelas_model->get_kelas_by_id($data_mhs->id_kelas);
+        $jadwal_kelas = $kelas_mhs->jadwal;
 
-        $jadwal_pbb = $dosen_pbb['jadwal'];
+        $jadwal_pbb = $dosen_pbb->jadwal;
+        $jadwal_pnj1 = $dosen_pnj1->jadwal;
+        $jadwal_pnj2 = $dosen_pnj2->jadwal;
 
-        $jadwal_pnj1 = $dosen_pnj1['jadwal'];
-
-        $jadwal_pnj2 = $dosen_pnj2['jadwal'];
+        $result = array();
 
         for ($i = 0; $i < 5; $i++) {
             for ($j = 0; $j < 7; $j++) {
