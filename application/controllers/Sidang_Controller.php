b06bc211971011541a38d4c4c4c862d902dbf9a4
Rezky <rezky20ti@mahasiswa.pcr.ac.id>
2024-07-25 00:40:08 +0700

    Update Sidang Controller

--- application/controllers/Sidang_Controller.php
+++ application/controllers/Sidang_Controller.php
@@ -14,7 +14,7 @@ class Sidang_Controller extends CI_Controller {
 
     public function find_recom() {
         $data = json_decode(file_get_contents('php://input'), true);
-        $tipe_sidang = $data['tipe_sidang']; // Get the type of schedule
+        $tipe_sidang = $data['tipe_sidang'];
     
         $data_mhs = $this->Mahasiswa_model->get_by_nim($data['mahasiswa']);
         $dosen_pbb = $this->Dosen_model->get_by_name($data['dosen_pbb']);
@@ -34,35 +34,20 @@ class Sidang_Controller extends CI_Controller {
         // Check if arrays are properly initialized and have the expected dimensions
         for ($i = 0; $i < 5; $i++) {
             for ($j = 0; $j < 7; $j++) {
-                $kelas_val = $jadwal_kelas[$i][$j];
-                $pbb_val = $jadwal_pbb[$i][$j];
-                $pnj1_val = $jadwal_pnj1[$i][$j];
-                $pnj2_val = $jadwal_pnj2[$i][$j];
-    
-                // Cast the result of the AND operation to an integer
-                $result[$i][$j] = (int)($kelas_val && $pbb_val && $pnj1_val && $pnj2_val);
-            }
-        }
-    
-        if ($tipe_sidang == "akhir") {
-            for ($i = 0; $i < 5; $i++) {
-                for ($j = 0; $j < 7; $j++) {
-                    if ($result[$i][$j] == 1) {
-                        // If the element is "1", set it to "1,1" (represented by 1 for both slots)
-                        $result[$i][$j] = 1;
-                    } else {
-                        // If the element is "0", set it to "0,0" (represented by 0 for both slots)
-                        $result[$i][$j] = 0;
+                if ($tipe_sidang == "akhir") {
+                    // Check pairs of hours
+                    if ($j == 0 || $j == 1) {
+                        $result[$i][0] = (int)($jadwal_kelas[$i][0] && $jadwal_pbb[$i][0] && $jadwal_pnj1[$i][0] && $jadwal_pnj2[$i][0]);
+                    } elseif ($j == 2 || $j == 3) {
+                        $result[$i][2] = (int)($jadwal_kelas[$i][2] && $jadwal_pbb[$i][2] && $jadwal_pnj1[$i][2] && $jadwal_pnj2[$i][2]);
+                    } elseif ($j == 4 || $j == 5) {
+                        $result[$i][4] = (int)($jadwal_kelas[$i][4] && $jadwal_pbb[$i][4] && $jadwal_pnj1[$i][4] && $jadwal_pnj2[$i][4]);
+                    } elseif ($j == 6) {
+                        $result[$i][5] = (int)($jadwal_kelas[$i][5] && $jadwal_pbb[$i][5] && $jadwal_pnj1[$i][5] && $jadwal_pnj2[$i][5]);
                     }
-    
-                    // Special condition for first element in each array
-                    if ($j == 0) {
-                        if ($result[$i][$j] == 1) {
-                            $result[$i][$j] = [1, 1];
-                        } else {
-                            $result[$i][$j] = [0, 0];
-                        }
-                    }
+                } else {
+                    // Regular check for each hour
+                    $result[$i][$j] = (int)($jadwal_kelas[$i][$j] && $jadwal_pbb[$i][$j] && $jadwal_pnj1[$i][$j] && $jadwal_pnj2[$i][$j]);
                 }
             }
         }
@@ -76,11 +61,18 @@ class Sidang_Controller extends CI_Controller {
     
             for ($i = 0; $i < 5; $i++) {
                 for ($j = 0; $j < 7; $j++) {
-                    if (is_array($result[$i][$j])) {
-                        $temp_result[$i][$j] = array_map(function($val) use ($ruangan_val, $i, $j) {
-                            return (int)($val && $ruangan_val[$i][$j]);
-                        }, $result[$i][$j]);
-                        $count_ones += array_sum($temp_result[$i][$j]);
+                    if ($tipe_sidang == "akhir") {
+                        // Check pairs of hours
+                        if ($j == 0 || $j == 1) {
+                            $temp_result[$i][0] = (int)($result[$i][0] && $ruangan_val[$i][0]);
+                        } elseif ($j == 2 || $j == 3) {
+                            $temp_result[$i][2] = (int)($result[$i][2] && $ruangan_val[$i][2]);
+                        } elseif ($j == 4 || $j == 5) {
+                            $temp_result[$i][4] = (int)($result[$i][4] && $ruangan_val[$i][4]);
+                        } elseif ($j == 6) {
+                            $temp_result[$i][5] = (int)($result[$i][5] && $ruangan_val[$i][5]);
+                        }
+                        $count_ones += $temp_result[$i][$j];
                     } else {
                         $temp_result[$i][$j] = (int)($result[$i][$j] && $ruangan_val[$i][$j]);
                         if ($temp_result[$i][$j] == 1) {
@@ -112,5 +104,6 @@ class Sidang_Controller extends CI_Controller {
         $this->output
             ->set_content_type('application/json')
             ->set_output(json_encode($response));
-    }    
+    }
+        
 }
