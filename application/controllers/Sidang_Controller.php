f31626786cd78f5e5325c679b83e3859a931eaf2
Rezky <rezky20ti@mahasiswa.pcr.ac.id>
2024-08-13 01:20:03 +0700

    Sidang Update

--- application/controllers/Sidang_Controller.php
+++ application/controllers/Sidang_Controller.php
@@ -87,7 +87,7 @@ class Sidang_Controller extends CI_Controller
             }
         }
 
-        $jadwal_ruangan->jadwal = json_encode($jadwal_ruangan->jadwal[$dayOfWeek]);
+        $jadwal_ruangan->jadwal = json_decode($jadwal_ruangan->jadwal[$dayOfWeek]);
 
         // Convert the result array to a JSON string
         $response['jadwal_without_ruangan'] = json_encode($result);
